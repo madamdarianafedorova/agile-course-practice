9b42dd9dfebd32a3e416f1f00ee9ae1075cc710b
Lenferd <DeysonSH@gmail.com>
2019-12-17 18:34:16 +0300

    Show view

--- code/bit-array/View/src/main/java/ru/unn/agile/bitarray/view/Main.java
+++ code/bit-array/View/src/main/java/ru/unn/agile/bitarray/view/Main.java
@@ -1,33 +1,22 @@
 package ru.unn.agile.bitarray.view;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class Main extends Application{
-
-    public static void main(String[] args) {
-
-        Application.launch(args);
-    }
+public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(final Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("BitArray.fxml"));
+        primaryStage.setTitle("Bit Array");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
-        // установка надписи
-        Text text = new Text("Hello from JavaFX!");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(100);   // установка положения надписи по оси X
-
-        Group group = new Group(text);
-
-        Scene scene = new Scene(group);
-        stage.setScene(scene);
-        stage.setTitle("First Application");
-        stage.setWidth(300);
-        stage.setHeight(250);
-        stage.show();
+    public static void main(final String[] args) {
+        launch(args);
     }
 }
