b72632a3cacf0e2fdf512f166e0d0a24831c9f60
dmitrygeralt <dmitry_nv@mail.ru>
2019-12-19 23:09:55 +0300

    fix  name

--- code/korniakov-kirill-lab2/ViewModel/src/test/java/ru/unn/agile/ComplexNumber/viewmodel/ViewModelTests.java
+++ code/korniakov-kirill-lab2/ViewModel/src/test/java/ru/unn/agile/ComplexNumber/viewmodel/ViewModelTests.java
@@ -21,7 +21,7 @@ public class ViewModelTests {
     }
 
     @Test
-    public void canSetDefaultValues() {
+    public void setsEmptyStingToInputByDefault() {
         assertEquals("", viewModel.re1Property().get());
         assertEquals("", viewModel.im1Property().get());
         assertEquals("", viewModel.re2Property().get());
